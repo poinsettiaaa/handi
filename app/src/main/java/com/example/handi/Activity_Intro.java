3c7586afe9a4b18fc41a11e3cfd7bd5d24120b81
poinsettiaaa <gkfnl1004>
2019-11-25 18:08:53 +0900

    handi

--- app/src/main/java/com/example/handi/Activity_Intro.java
+++ app/src/main/java/com/example/handi/Activity_Intro.java
@@ -1,6 +1,10 @@
 package com.example.handi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,25 @@ public class Activity_Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+
+
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                //startActivity(intent);
+                startActivity(new Intent(Activity_Intro.this, Activity_Intro.class));
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+}
+
+public class Activity_Login extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
     }
 }

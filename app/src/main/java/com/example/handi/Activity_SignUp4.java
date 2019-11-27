cb05cc6a3c89e692766a518d003e129b8b44b550
poinsettiaaa <gkfnl1004>
2019-11-27 17:44:59 +0900

    handi

---
+++ app/src/main/java/com/example/handi/Activity_SignUp4.java
@@ -0,0 +1,17 @@
+package com.example.handi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_SignUp4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up4);
+
+
+    }
+
+}

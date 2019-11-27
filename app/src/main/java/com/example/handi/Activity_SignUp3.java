6a15a57d824acb3cd67a7fd23e74e921fccf6441
poinsettiaaa <gkfnl1004>
2019-11-27 17:52:14 +0900

    handi

--- app/src/main/java/com/example/handi/Activity_SignUp3.java
+++ app/src/main/java/com/example/handi/Activity_SignUp3.java
@@ -1,14 +1,19 @@
 package com.example.handi;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity_SignUp3 extends AppCompatActivity {
+
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,18 @@ public class Activity_SignUp3 extends AppCompatActivity {
         setContentView(R.layout.sign_up3);
 
 
+        btnNext = findViewById(R.id.btnNext);
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //클릭시
+            public void onClick(View view) {
+                startActivity(new Intent(Activity_SignUp3.this, Activity_SignUp4.class));
+            }
+
+
+        });
     }
 
 }

2a80af8708ed27bb6f3efc58f62714bdb6cd6926
poinsettiaaa <gkfnl1004>
2019-11-28 19:32:25 +0900

    handi

--- app/src/main/java/com/example/handi/Activity_Intro.java
+++ app/src/main/java/com/example/handi/Activity_Intro.java
@@ -1,18 +1,39 @@
 package com.example.handi;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.security.MessageDigest;
+
 public class Activity_Intro extends AppCompatActivity {
+
+    private Context mContext;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+
+
+        //해시키
+        mContext = getApplicationContext();
+        getHashKey(mContext);
+
+
+
+
 
 
         Handler handler = new Handler() {
@@ -25,4 +46,59 @@ public class Activity_Intro extends AppCompatActivity {
         };
         handler.sendEmptyMessageDelayed(0, 3000);
     }
+
+
+    // 프로젝트의 해시키를 반환
+
+    @Nullable
+    public static String getHashKey(Context context) {
+
+        final String TAG = "KeyHash";
+
+        String keyHash = null;
+
+        try {
+
+            PackageInfo info =
+
+                    context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+
+
+
+            for (Signature signature : info.signatures) {
+
+                MessageDigest md;
+
+                md = MessageDigest.getInstance("SHA");
+
+                md.update(signature.toByteArray());
+
+                keyHash = new String(Base64.encode(md.digest(), 0));
+
+                Log.d(TAG, keyHash);
+
+            }
+
+        } catch (Exception e) {
+
+            Log.e("name not found", e.toString());
+
+        }
+
+
+
+        if (keyHash != null) {
+
+            return keyHash;
+
+        } else {
+
+            return null;
+
+        }
+
+    }
+
 }
+
+
--- app/src/main/java/com/example/handi/Activity_Login.java
+++ app/src/main/java/com/example/handi/Activity_Login.java
@@ -2,20 +2,52 @@ package com.example.handi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
 
 public class Activity_Login extends AppCompatActivity {
 
     private TextView tvSignup;
 
+    private ISessionCallback callback;
+
+    //카카오 버튼
+    private Button btn_custom_login;
+    private LoginButton btn_kakao_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        callback = new ISessionCallback() {
+            @Override
+            public void onSessionOpened() {
+                Log.d("111111","4444444");
+                requestMe();
+            }
+
+            @Override
+            public void onSessionOpenFailed(KakaoException exception) {
+
+                Log.d("111111","3333333");
+            }
+        };
+
+        Session.getCurrentSession().addCallback(callback);
 
         tvSignup = findViewById(R.id.tvSignup);
 
@@ -26,6 +58,46 @@ public class Activity_Login extends AppCompatActivity {
                 startActivity(new Intent(Activity_Login.this, Activity_SignUp.class));
             }
 
+        });
+
+
+
+        btn_kakao_login = (LoginButton) findViewById(R.id.btn_kakao_login);
+        //카카오 로그인
+        btn_custom_login = (Button) findViewById(R.id.btn_custom_login);
+        btn_custom_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("1111111","111111");
+
+                btn_kakao_login.performClick();
+            }
+
+        });
+
+    }
+    private void requestMe() {
+
+        Log.d("111111","22222222");
+
+
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+
+            }
+
+            @Override
+            public void onNotSignedUp() {
+
+            }
+
+            @Override
+            public void onSuccess(UserProfile result) {
+
+                Log.d("result :",result.getNickname());
+
+            }
         });
 
 
